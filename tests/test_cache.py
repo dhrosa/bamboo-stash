adaaf11bfb22dcb7349562e91f2b9eca5f4bce8f
Diony Rosa <dhrosa@gmail.com>
2024-03-28 17:20:56 -0400

    Explicitly test Series and DataFrame arguments

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+from shutil import rmtree
+from typing import cast
 
+from pandas import DataFrame, Series
 from pytest import fixture
 
 from bamboo_stash import Cache
@@ -38,3 +41,51 @@ def test_args(cache: Cache) -> None:
     assert f(2) == 4
     assert f(1) == 1
     assert call_count == 2
+
+
+def test_cache_file_deletion(cache: Cache) -> None:
+    call_count = 0
+
+    @cache
+    def f() -> int:
+        nonlocal call_count
+        call_count += 1
+        return 4
+
+    assert f() == 4
+    assert f() == 4
+    assert call_count == 1
+
+    rmtree(cache.base_dir)
+    assert f() == 4
+    assert call_count == 2
+
+
+def test_series_arg(cache: Cache) -> None:
+    call_count = 0
+
+    @cache
+    def f(s: "Series[int]") -> int:
+        nonlocal call_count
+        call_count += 1
+        return s.sum()
+
+    s = Series([1, 2, 3])
+    assert f(s) == 6
+    assert f(s.copy()) == 6
+    assert call_count == 1
+
+
+def test_dataframe_arg(cache: Cache) -> None:
+    call_count = 0
+
+    @cache
+    def f(df: DataFrame) -> int:
+        nonlocal call_count
+        call_count += 1
+        return cast(int, df.sum().sum())
+
+    df = DataFrame(data=[[1, 2, 3], [4, 5, 6]])
+    assert f(df) == 21
+    assert f(df.copy()) == 21
+    assert call_count == 1
